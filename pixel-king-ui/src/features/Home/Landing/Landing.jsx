80288f7564f2e95c7b2fdf5ef2e41fa7663043c6
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-05-04 03:09:54 +0300

    Design updates

--- pixel-king-ui/src/features/Home/Landing/Landing.jsx
+++ pixel-king-ui/src/features/Home/Landing/Landing.jsx
@@ -3,7 +3,7 @@ import landingSideImages from '../../../assets/landing_images.png'
 
 export default function Landing() {
     return (
-        <section className={styles.landing__container}>å
+        <section className={styles.landing__container}>
                 <div className={styles.landing__shadow__1}></div>
                 <div className={styles.landing__shadow__2}></div>
             <h1 className={styles.landing__main__h1}>INSPIRE<br/>CREATE<br/>ADORN</h1>
