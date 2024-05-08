58c095f95b70e2620107baf92005bbce9605fe53
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-05-08 03:44:52 +0300

    Admin preparation

--- pixel-king-ui/src/App.jsx
+++ pixel-king-ui/src/App.jsx
@@ -3,17 +3,19 @@ import Header from './core/header/header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./features/Home/Home";
 import Wallpapers from "./features/Wallpapers/Wallpapers";
+import AdminLogin from "./features/AdminLogin/AdminLogin";
 
 function App() {
     return (
         <BrowserRouter>
             <>
-                <Header />
+                {window.location.pathname.indexOf('/admin') === -1 && <Header />}
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
                     <Route path="/wallpapers" element={<Wallpapers />}></Route>
-                    <Route path="/license" element={<h1>License Route</h1>}></Route>
-                    <Route path="/contact" element={<h1>Contact Route</h1>}></Route>
+                    <Route path="/license" element={<h1>License Route</h1>} />
+                    <Route path="/contact" element={<h1>Contact Route</h1>} />
+                    <Route path="/admin/login" element={<AdminLogin />} />
                 </Routes>
             </>
         </BrowserRouter>
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const dotenv = require('dotenv');
+const { Surreal } = require('surrealdb.node');
+const db = new Surreal();
+dotenv.config()
 const routes = require('./routes');
 const cors = require('cors');
 
@@ -8,8 +12,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+async function connect() {
+    try {
+        await db.connect('ws:0.0.0.0:8000')
+
+        await db.signin({
+            username: 'admin',
+            password: 'pixel070219',
+        });
+
+        await db.use({ ns: 'pixelking', db: 'pixelking' });
+
+        console.log('Connected to database');
+    } catch {
+        console.log('Failed to connect to database');
+    }
+}
+
+connect();
+
 app.use(routes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
