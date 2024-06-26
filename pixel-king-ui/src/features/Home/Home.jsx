19c1e2319438082feeaf40d2e15bab7252143b24
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-06-26 16:04:06 +0300

    Added static image comparing

--- pixel-king-ui/src/features/Home/Home.jsx
+++ pixel-king-ui/src/features/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Landing from './Landing/Landing';
 import GetStarted from "./GetStarted/GetStarted";
+import CompareImages from "./CompareImages/CompareImages";
 
 export default function Home() {
     return (
         <>
             <Landing />
             <GetStarted />
+            <CompareImages />
         </>
     )
 }
