b7496de07e4d4403a06381c8f6d5bcbd089d26a7
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-08-09 12:30:13 +0300

    bug fixes

--- pixel-king-ui/src/App.jsx
+++ pixel-king-ui/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './core/header/header';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Home from "./features/Home/Home";
 import License from "./features/License/License";
 import Wallpapers from "./features/Wallpapers/Wallpapers";
@@ -15,40 +15,48 @@ import Contact from "./features/Contact/Contact";
 import Footer from "./core/footer/footer";
 import Privacy from "./features/Privacy/Privacy";
 
+function AppLayout() {
+    const location = useLocation();
+    const isAdminRoute = location.pathname.startsWith('/admin');
+
+    return (
+        <>
+            {!isAdminRoute && <Header />}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/wallpapers" element={<Wallpapers />} />
+                <Route path="/license" element={<License />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path='/privacy' element={<Privacy />} />
+                <Route path="/admin/login" element={<AdminLogin />} />
+                <Route path="/admin/*" element={
+                    <ProtectedRoute>
+                        <AdminLayout />
+                    </ProtectedRoute>
+                }>
+                    <Route path="wallpapers" element={<ManageWallpapers />} />
+                    <Route path="emails" element={
+                        <ProtectedRoute roles={['admin']}>
+                            <ManageEmails />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="users" element={
+                        <ProtectedRoute roles={['admin']}>
+                            <ManageUsers />
+                        </ProtectedRoute>
+                    } />
+                </Route>
+            </Routes>
+            {!isAdminRoute && <Footer />}
+        </>
+    );
+}
+
 function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <>
-                    {window.location.pathname.indexOf('/admin') === -1 && <Header />}
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/wallpapers" element={<Wallpapers />} />
-                        <Route path="/license" element={<License />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path='/privacy' element={<Privacy />} />
-                        <Route path="/admin/login" element={<AdminLogin />} />
-                        <Route path="/admin/*" element={
-                            <ProtectedRoute>
-                                <AdminLayout />
-                            </ProtectedRoute>
-                        }>
-                            <Route path="wallpapers" element={<ManageWallpapers />} />
-                            <Route path="emails" element={
-                                <ProtectedRoute roles={['admin']}>
-                                    <ManageEmails />
-                                </ProtectedRoute>
-                            } />
-                            <Route path="users" element={
-                                <ProtectedRoute roles={['admin']}>
-                                    <ManageUsers />
-                                </ProtectedRoute>
-                            } />
-                        </Route>
-                    </Routes>
-                    {window.location.pathname.indexOf('/admin') === -1 && <Footer />}
-
-                </>
+                <AppLayout />
             </AuthProvider>
         </BrowserRouter>
     );
