c126b7d4cf50a464d86858b8fa11e9908a6e23cd
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-09-05 13:12:55 +0300

    Show pixels in header

--- pixel-king-ui/src/core/header/header.jsx
+++ pixel-king-ui/src/core/header/header.jsx
@@ -1,12 +1,35 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import logo from '../../assets/logo.png';
+import pixelsImg from '../../assets/Diamond.png'
 import styles from './header.module.css';
 import { Link } from 'react-router-dom';
-import AuthContext from '../../auth/AuthContext'; // Import the AuthContext
+import AuthContext from '../../auth/AuthContext';
+import axios from "axios";
 
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [pixels, setPixels] = useState('...');
     const { isUserAuthenticated, userLogout } = useContext(AuthContext);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/users/account-details`, {
+                    headers: {
+                        Authorization: localStorage.getItem('userToken')
+                    }
+                });
+                setPixels(response.data.credits);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+
+        return () => {
+        };
+    }, []);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
@@ -19,29 +42,38 @@ export default function Header() {
 
     return (
         <header className={styles.header}>
-            <Link to='/'>
-                <div className={styles.header__logo_container}>
-                    <img className={styles.header__logo} src={logo} alt="Pixel King" />
-                    <p className={styles.header_logo_text}>pixel king</p>
-                </div>
-            </Link>
+            <div className={styles.header__left}>
+                <Link to='/'>
+                    <div className={styles.header__logo_container}>
+                        <img className={styles.header__logo} src={logo} alt="Pixel King" />
+                        <p className={styles.header_logo_text}>pixel king</p>
+                    </div>
+                </Link>
+                <nav>
+                    <ul className={styles.header__nav__list}>
+                        <li><Link className={styles.header__link} to="/">Home</Link></li>
+                        <li><Link className={styles.header__link} to="/upgrade">Premium</Link></li>
+                        <li><Link className={styles.header__link} to="/contact">Contact</Link></li>
+                        {isUserAuthenticated ? (
+                            <>
+                                <li><Link className={styles.header__link} to="/account">Account</Link></li>
+                                <li><span className={styles.header__link} onClick={handleLogout}>Logout</span></li>
+                            </>
+                        ) : (
+                            <>
+                                <li><Link className={styles.header__link} to="/login">Login</Link></li>
+                                <li><Link className={styles.header__link} to="/register">Register</Link></li>
+                            </>
+                        )}
+                    </ul>
+                </nav>
+            </div>
             <nav>
-                <ul className={styles.header__nav__list}>
-                    <li><Link className={styles.header__link} to="/">Home</Link></li>
-                    <li><Link className={styles.header__link} to="/upgrade">Premium</Link></li>
-                    <li><Link className={styles.header__link} to="/contact">Contact</Link></li>
-                    {isUserAuthenticated ? (
-                        <>
-                            <li><Link className={styles.header__link} to="/account">Account</Link></li>
-                            <li><span className={styles.header__link} onClick={handleLogout}>Logout</span></li>
-                        </>
-                    ) : (
-                        <>
-                            <li><Link className={styles.header__link} to="/login">Login</Link></li>
-                            <li><Link className={styles.header__link} to="/register">Register</Link></li>
-                        </>
-                    )}
-                    <li><Link className={styles.header__link} to="/wallpapers">Wallpapers</Link></li> {/* Wallpapers moved to last */}
+                <ul className={styles.wallpapers_nav}>
+                    <li className={styles.pixels__container}>
+                        <p><img src={pixelsImg}/>{pixels}</p>
+                    </li>
+                    <li><Link className={`${styles.header__link} ${styles.header__special}`} to="/wallpapers">Wallpapers</Link></li>
                 </ul>
             </nav>
             <div className={styles.mobile_nav}>
@@ -54,7 +86,7 @@ export default function Header() {
                     <div className={styles.close} onClick={toggleMenu}>&times;</div>
                     <ul className={styles.mobile_menu_list}>
                         <li><Link className={styles.mobile_link} to="/" onClick={toggleMenu}>Home</Link></li>
-                        <li><Link className={styles.mobile_link} to="/wallpapers" onClick={toggleMenu}>Wallpapers</Link></li>
+                        <li><Link className={`${styles.mobile_link} ${styles.mobile_special}`} to="/wallpapers" onClick={toggleMenu}>Wallpapers</Link></li>
                         <li><Link className={styles.mobile_link} to="/license" onClick={toggleMenu}>License</Link></li>
                         <li><Link className={styles.mobile_link} to="/contact" onClick={toggleMenu}>Contact</Link></li>
                         {isUserAuthenticated ? (
